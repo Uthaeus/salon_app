407ef95c32587127cb0e992085a4168afba841c4
Uthaeus <Uth@github.com>
2018-02-20 13:12:16 -0700

    fixed bug on product index

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,23 +4,17 @@ module ProductsHelper
     "http://via.placeholder.com/600x400"
   end
 
-  # def product_img img
-  #   if img?
-  #     img
-  #   else 
-  #     image_generator(height: '600', width: '400')
-  #   end
-  # end
+  def product_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    elsif type == 'main'
+      image_generator(height: '600', width: '400')
+    end
+  end
+
 end
 
 
 
-# def product_img img, type
-#   if img.model.main_image? || img.model.thumb_image?
-#     img
-#   elsif type == 'thumb'
-#     image_generator(height: '350', width: '200')
-#   elsif type == 'main'
-#     image_generator(height: '600', width: '400')
-#   end
-# end
